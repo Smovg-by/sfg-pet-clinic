b25a967d9ec70a7323de75e4ab3ee1a7efa1fcba
Uladzimir Smouzh <USmouzh@ibagroup.eu>
2023-07-31 22:22:45 +0300

    Implement Base Entity (JPA). Closes #35

--- pet-clinic-data/src/main/java/smvg/spring/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/smvg/spring/sfgpetclinic/model/BaseEntity.java
@@ -1,8 +1,16 @@
 package smvg.spring.sfgpetclinic.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
 import java.io.Serializable;
 
+@MappedSuperclass //так мы говорим Спрингу, что для этого класса не надо создавать таблицу, т.к. от этого класса мы будем наследовать остальные
 public class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
@@ -11,5 +19,9 @@ public class BaseEntity implements Serializable {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public boolean isNew() {
+        return this.id == null;
     }
 }
